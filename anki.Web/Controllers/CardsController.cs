92c24cda9ceb03f0af01ae44c163bc21dee15715
pavlodenshchyk <bloody.squirr3l@gmail.com>
2022-12-02 22:24:41 +0200

    add web solution

---
+++ anki.Web/Controllers/CardsController.cs
@@ -0,0 +1,23 @@
+﻿using anki.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace anki.Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CardsController : ControllerBase
+{
+    private CardService _cardService;
+
+    public CardsController()
+    {
+        _cardService = new CardService(new CardRepository());
+    }
+    
+    [HttpGet]
+    public IEnumerable<Card> Get()
+    {
+        return _cardService.GetAll();
+    }
+}
