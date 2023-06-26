6359eed411974d6d4871e4e28077bbca42ab5ca4
pavlodenshchyk <bloody.squirr3l@gmail.com>
2023-06-26 15:57:02 +0300

    add material and some changes in nav-menu

--- anki.Web/ClientApp/src/app/app.module.ts
+++ anki.Web/ClientApp/src/app/app.module.ts
@@ -7,28 +7,40 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from "@angular/material/card";
+import {MatMenuModule} from "@angular/material/menu";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     HttpClientModule,
+    MatSlideToggleModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      {path: '', component: HomeComponent, pathMatch: 'full'},
+    ]),
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatCardModule,
+    MatCardModule,
+    MatCardModule,
+    MatCardModule,
+    MatCardModule,
+    MatMenuModule,
+    MatMenuModule,
+    MatMenuModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
